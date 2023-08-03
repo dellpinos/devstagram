61e08a6bc9e6b7349e8ad601b4481ec534a69136
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-08-03 14:47:22 -0300

    finalizando crud de publicaciones

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class PostController extends Controller
 {
@@ -53,6 +54,23 @@ class PostController extends Controller
             'post' => $post,
             'user' => $user
         ]);
+    }
+
+    public function destroy(Post $post)
+    {
+        // Eliminar registro
+        $this->authorize('delete', $post);
+        $post->delete();
+
+        // Eliminar imagen
+        $imagen_path = public_path('uploads/' . $post->imagen);
+
+        if(File::exists($imagen_path)) {
+            unlink($imagen_path);
+        }
+
+
+        return redirect()->route('posts.index', auth()->user()->username);
     }
 
 }
