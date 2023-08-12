5d6ee0d2c5bb1c706da527338d57eef3e489bf38
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-08-12 00:42:46 -0300

    Finalizando Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,29 @@
 @extends('layouts.app')
 
 @section('titulo')
-
-Esta es la homepage
-
+    Esta es la homepage
 @endsection
 
 @section('contenido')
 
-Este es "el contenido"
+    @if ($posts->count())
+        <div class=" grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            @foreach ($posts as $post)
+                <div>
+                    <a href="{{ route('posts.show', ['post' => $post, 'user' => $post->user]) }}">
+                        <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
+                    </a>
+                </div>
+            @endforeach
+        </div>
+
+        <div class=" my-10">
+            {{ $posts->links() }}
+        </div>
+
+    @else
+        <p class=" text-center">No hay posts</p>
+
+    @endif
 
 @endsection
